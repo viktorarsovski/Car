e04cbb8e98a45faa72fc3e548b2f3f115cfa2e69
Viktor <macedonian.wow@live.com>
2020-03-05 14:44:08 +0100

    First Commit

--- lib/car.rb
+++ lib/car.rb
@@ -2,13 +2,17 @@ require "car/version"
 
 module Car
   class MyCar
-    attr_accessor :color, :car_speed
+    attr_accessor :color, :speed
     attr_reader :year
     def initialize(model, year, color)
       @model = model
       @year = year
       @color = color
-      @car_speed = 0
+      @speed = 0
+    end
+
+    def speed_up 
+      @speed += 50
     end
   end
 end
--- spec/mycar_spec.rb
+++ spec/mycar_spec.rb
@@ -1,11 +1,27 @@
-RSpec.describe Car::MyCar
-   it "creates instance of MyCar class" do
-    volvo = Car::MyCar.new('volvo', '2020', 'blue')
-    expect(volvo).to be_instance_of(Car::MyCar)
-   end
+RSpec.describe Car::MyCar do
+  it "creates instance of MyCar class" do
+  audi = Car::MyCar.new('audi', 'blue', 2020)
+  expect(audi).to be_instance_of(Car::MyCar)
+  end
+    
+  it "has starting speed defined to 0" do
+  audi = Car::MyCar.new('audi', 'blue', 2020)
+  expect(audi.speed).to eq(0)
+  end
 
-   it "has starting speed defined to 0" do
-    volvo = Car::MyCar.new('volvo', '2020', 'blue')
-    expect(volvo.car_speed).to eq(0)
-
+  it "increases current speed for 50 km/h" do
+  audi = Car::MyCar.new("audi", "blue", 2020)  
+  expect(audi.speed_up).to be(50)
+  end
+    
+  it "increases current speed for 100 km/h" do
+  audi = Car::MyCar.new("audi", "blue", 2020) 
+  expect(audi.speed_up * 2).to be(100)
+  end
+    
+  it "increases current speed for 150 km/h" do
+  audi = Car::MyCar.new("audi", "blue", 2020) 
+  expect(audi.speed_up * 3).to be(150)
+  end
 end
+    
