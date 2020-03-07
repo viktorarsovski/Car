f51e9389ab80025797fa622ef0aa8c10317b2d3d
Viktor <macedonian.wow@live.com>
2020-03-07 15:11:29 +0100

    First Commit

--- lib/car.rb
+++ lib/car.rb
@@ -2,17 +2,37 @@ require "car/version"
 
 module Car
   class MyCar
+    SPEED_OFFSET = 15
+
     attr_accessor :color, :speed
-    attr_reader :year
+    attr_reader :year, :defects
     def initialize(model, year, color)
       @model = model
       @year = year
       @color = color
       @speed = 0
+      @defects = []
     end
 
-    def speed_up 
-      @speed += 50
+    def speed_up(increase = SPEED_OFFSET)
+      @speed += increase
+    end
+
+    def slow_down(decrease = SPEED_OFFSET)
+      @speed -= decrease
+    end
+
+    def spray_paint(arg)
+      @color = arg
+    end
+
+    def add_defect(defect)
+      @defects.push(defect)
+    end
+
+    def service_info
+      defects = @defects.join(', ')
+      "Defects to be fixed: #{defects}."
     end
   end
 end
--- spec/mycar_spec.rb
+++ spec/mycar_spec.rb
@@ -1,27 +1,52 @@
 RSpec.describe Car::MyCar do
+  let(:audi) { Car::MyCar.new('audi', 'blue', 2020) }
+  let(:failure) { 'water pump' }
+
   it "creates instance of MyCar class" do
-  audi = Car::MyCar.new('audi', 'blue', 2020)
-  expect(audi).to be_instance_of(Car::MyCar)
-  end
-    
-  it "has starting speed defined to 0" do
-  audi = Car::MyCar.new('audi', 'blue', 2020)
-  expect(audi.speed).to eq(0)
+    expect(audi).to be_instance_of(Car::MyCar)
   end
 
-  it "increases current speed for 50 km/h" do
-  audi = Car::MyCar.new("audi", "blue", 2020)  
-  expect(audi.speed_up).to be(50)
+  it "has starting speed defined to 0" do
+    expect(audi.speed).to eq(0)
   end
-    
+
+  it "increases current speed for 10 km/h" do
+    audi.speed_up
+    expect(audi.speed).to eq(Car::MyCar::SPEED_OFFSET)
+  end
+
+  it "increases current speed for 40 km/h" do
+    audi.speed_up(40)
+    expect(audi.speed).to eq(40)
+  end
+
   it "increases current speed for 100 km/h" do
-  audi = Car::MyCar.new("audi", "blue", 2020) 
-  expect(audi.speed_up * 2).to be(100)
+     audi.speed_up(100)
+     expect(audi.speed).to eq(100)
   end
-    
-  it "increases current speed for 150 km/h" do
-  audi = Car::MyCar.new("audi", "blue", 2020) 
-  expect(audi.speed_up * 3).to be(150)
+
+  it "slows down" do
+    audi.slow_down
+    expect(audi.speed).to eq(-Car::MyCar::SPEED_OFFSET)
+  end
+
+  it 'changes the color of the object' do
+    audi.spray_paint :black
+    expect(audi.color).to eq(:black)
+  end
+
+  it 'has no defects' do
+    expect(audi.defects).not_to include(failure)
+  end
+
+  it 'adds defects' do
+    audi.add_defect(failure)
+    expect(audi.defects).to include(failure)
+  end
+
+  it 'lists all the defects' do
+    audi.add_defect(failure)
+    audi.add_defect('oil leak')
+    expect(audi.service_info).to eq("Defects to be fixed: #{failure}, oil leak.")
   end
 end
-    
